d92bed4b24f1ffed4aadff8ccce730c18820636b
David Petterson <david.petterson@weightwatchers.com>
2019-03-16 12:45:39 -0400

    Initial commit

---
+++ build/precache-manifest.de0919af221fdd154e0853051d1676a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f0d27aedf721858d424",
+    "url": "/static/js/main.0f0d27ae.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "0f0d27aedf721858d424",
+    "url": "/static/css/main.9873571e.chunk.css"
+  },
+  {
+    "revision": "8a0c86dae346a96a3cf1a5f43023c44e",
+    "url": "/index.html"
+  }
+];
